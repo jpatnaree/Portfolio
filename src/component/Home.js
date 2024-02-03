0a8f6b5bb06fb7d55a0a51231ce8cd02f8a91899
Patnaree Johnson <j.patnaree.t@gmail.com>
2024-02-02 21:35:08 -0500

    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -13,13 +13,13 @@ function Home() {
             <div className="namediv">
                 <h1 id='name'>Patnaree</h1>
                 <h1 id='fullname'>⟨ Pat ⟩ Johnson</h1>
+                <div className="linkinfo">
+                    <a href="https://github.com/jpatnaree"> <img src="image/github.png" alt='GitHub'/> </a>
+                    <a href="https://www.linkedin.com/in/patnaree-johnson-470643299/"> <img src="image/linkedin.png" alt='LinkedIn'/></a>
+                    <a href="mailto:jpatnaree@gmail.com"><img src="image/email.png" alt='Email'/></a>
+                </div>
             </div>
 
-            <div className="linkinfo">
-                <a href="https://github.com/jpatnaree"> <img src="image/github.png" alt='GitHub'/> </a>
-                <a href="https://www.linkedin.com/in/patnaree-johnson-470643299/"> <img src="image/linkedin.png" alt='LinkedIn'/></a>
-                <a href="mailto:jpatnaree@gmail.com"><img src="image/email.png" alt='Email'/></a>
-            </div>
 
         </div>
         </>
